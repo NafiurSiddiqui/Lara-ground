c57833b034eed3c9522c6c7309bedff2611c42f5
Nafi R <saejim91@gmail.com>
2023-07-08 03:49:23 +0600

    init

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/app.css">
+        <title>Posts</title>
+    </head>
+
+    <body class="">
+        <article>
+            <h1> <a href="/post">First Post</a> </h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, reprehenderit. Numquam ea vel
+                tenetur exercitationem nisi, dolor consequatur id quos voluptatum praesentium similique enim voluptates
+                alias at ullam accusamus ipsam? Iste quia ratione magnam delectus fugit veritatis deserunt dolores?
+                Possimus? </p>
+        </article>
+        <hr>
+        <article>
+            <h1> <a href="/post">Second Post</a> </h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, reprehenderit. Numquam ea vel
+                tenetur exercitationem nisi, dolor consequatur id quos voluptatum praesentium similique enim voluptates
+                alias at ullam accusamus ipsam? Iste quia ratione magnam delectus fugit veritatis deserunt dolores?
+                Possimus? </p>
+        </article>
+        <hr>
+        <article>
+            <h1> <a href="/post">Third Post</a> </h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, reprehenderit. Numquam ea vel
+                tenetur exercitationem nisi, dolor consequatur id quos voluptatum praesentium similique enim voluptates
+                alias at ullam accusamus ipsam? Iste quia ratione magnam delectus fugit veritatis deserunt dolores?
+                Possimus? </p>
+        </article>
+        <hr>
+    </body>
+
+</html>
