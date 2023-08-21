aa8df68c90b54560a04cdc32295fbb784bcb8a33
Nafi R <saejim91@gmail.com>
2023-08-22 01:13:46 +0600

    post markup serailized

---
+++ resources/views/post-lesson.blade.php
@@ -0,0 +1,24 @@
+<x-layout>
+    
+    <article>
+        <h1>
+            {{-- {!! $post->title !!} --}}
+            {{ $post->title }}
+        </h1>
+
+    
+            <div>
+                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            </div>
+
+
+        <p>
+            <!-- THIS IS HOW YOU PARSE HTML WITH BLADE -->
+            {!! $post->body !!}
+        </p>
+    
+    </article>
+
+    <a href="/posts">Go back</a>   
+
+</x-layout>
