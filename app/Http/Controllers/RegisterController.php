9e20f71cb140a1b19ec794f6d57c2a3d718ca3c1
Nafi R <saejim91@gmail.com>
2023-10-27 01:24:19 +0600

    register route created

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    public function create()
+    {
+        // return 'We can register an user!'; TEST
+
+        return view('register.create');
+    }
 }
